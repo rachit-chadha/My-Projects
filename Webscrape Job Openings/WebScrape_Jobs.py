ca99b8befcb24c323a45cb1525ea93187a3a1421
Rachit Chadha <rachitch@gmail.com>
2020-03-19 13:33:38 -0400

    commit 2

--- Webscrape Job Openings/WebScrape_Jobs.py
+++ Webscrape Job Openings/WebScrape_Jobs.py
@@ -1,10 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Sep  9 13:17:36 2019
-
-@author: adityabhat
-"""
 import pandas as pd
 from bs4 import BeautifulSoup
 from requests import get
@@ -14,7 +7,7 @@ from requests import get
 #list to store job metrics
 title = []
 company_name = []
-location = [] 
+location = []
 
 
 # variables used for modifying url parameters
@@ -27,41 +20,39 @@ headers = {"Accept-Language": "en-US, en;q=0.5"}
 
 #looping through locations
 for loc in locations:
-    #looping through jobs 
+    #looping through jobs
     for q in query:
-        
-        #making a get request 
+
+        #making a get request
         response=get("https://www.indeed.com/jobs?q=" + q + "&l=" + loc, headers = headers )
-        
+
         # Parse the content of the request with BeautifulSoup
         bs = BeautifulSoup(response.text, 'html.parser')
-        
+
         #Creating container for job listings
         job_container = bs.find_all('div', {'class':{'jobsearch-SerpJobCard'}})
-        
+
         for container in job_container:
-    
+
             #extracting job title
             position = container.find('a',{'class':'jobtitle turnstileLink '}).text
             title.append(position)
-            
+
             #extracting company name
             name=container.find('span',{'class':'company'}).text
             company_name.append(name)
-            
-            
+
+
             #extracting location
             place=container.find(['span','div'],{'class':{'location accessible-contrast-color-location'}})
             location.append(place.get_text())
-            
+
 #Creating dataframes for to store scraped data
 jobs_data = pd.DataFrame({'Title': title,
 'Company Name': company_name,
 'Location': location,
 })
-    
+
 print(jobs_data.info())
 jobs_data
 jobs_data.to_csv("/Users/Rachit/Desktop/jobs_data.csv")
-
-
